0f3c633fa20233cc23b00671b10474df8e797b76
Mariusz Smenżyk <mariusz.smenzyk@sizeof.pl>
2016-05-04 22:59:09 +0200

    v0.0.0

---
+++ module.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+
+    /**
+     * @ngdoc overview
+     * @name app [app.docs]
+     * @description Docs module of the application.
+     */
+    angular
+        .module('app.docs');
+    
+})();
