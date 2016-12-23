3e7e72653379d8f702ff9efc3ddefc8ac8320f92
Nikodem Kler SIZEOF <nikodem.kler@sizeof.pl>
2016-12-23 13:20:08 +0100

    Ignore statuses modification (hard coded list)

--- src/angular-brave-toastr.js
+++ src/angular-brave-toastr.js
@@ -8,15 +8,21 @@
    */
   angular
     .module('ngBraveToastr', [])
-    .value('version', '0.0.1')
+    .value('version', '0.0.2')
     .constant('config', {})
     .config(function ($httpProvider) {
 
       $httpProvider.interceptors.push(['$q', '$injector', function ($q, $injector) {
 
         function notifyError(rejection) {
-          var toastr = $injector.get('toastr');
-          toastr.error(rejection.status + ' ' + rejection.statusText, 'Error');
+
+          var ignoredStatuses = [400, 404]; // TODO nkler: inject array values with config provider
+          var isIgnored = (ignoredStatuses.indexOf(rejection.status) > -1);
+
+          if (isIgnored === false) {
+            var toastr = $injector.get('toastr');
+            toastr.error(rejection.status + ' ' + rejection.statusText, 'Error');
+          }
         }
 
         return {
